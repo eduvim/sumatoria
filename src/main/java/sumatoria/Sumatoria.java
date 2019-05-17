f38b5d6e525ae38f04e8fd60b6fc242f6ad4dca8
eduvim <evillasrmx@gmail.com>
2019-05-16 21:10:55 -0500

    Corrige caso de prueba

--- src/main/java/sumatoria/Sumatoria.java
+++ src/main/java/sumatoria/Sumatoria.java
@@ -3,6 +3,9 @@ package sumatoria;
 public class Sumatoria {
     public static Integer calcular(int numero) {
     	int sumatoria = 0;
+    	if( numero < 0 ) {
+    		throw new ArithmeticException("La sumatoria no se puede obtener sobre un numero negativo");
+    	}
     	for( int i = 0 ; i < numero ; i++) {
     		sumatoria += numero - i;
     	}
