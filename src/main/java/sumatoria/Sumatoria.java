69e6567cb508fcb1e339765cee1749115675aeac
eduvim <evillasrmx@gmail.com>
2019-05-16 21:24:20 -0500

    Modifica orden de if

--- src/main/java/sumatoria/Sumatoria.java
+++ src/main/java/sumatoria/Sumatoria.java
@@ -2,14 +2,14 @@ package sumatoria;
 
 public class Sumatoria {
     public static Integer calcular(int numero) {
+    	if ( numero == 1 ) {
+    		return 1;
+    	}
     	if( numero < 0 ) {
     		throw new ArithmeticException("La sumatoria no se puede obtener sobre un numero negativo");
     	}
     	if( numero == 0) {
     		return 0;
-    	}
-    	if ( numero == 1 ) {
-    		return 1;
     	}
     	  return numero  + calcular(numero -1);
     }
